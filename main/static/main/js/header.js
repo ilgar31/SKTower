f80cd475ae694e41ceb488d7d273761caa142257
ilgar31 <ilgar.bagishev@gmail.com>
2024-05-04 19:16:53 +0300

    added mobile header, mobile header manu and first block of the home page

--- main/static/main/js/header.js
+++ main/static/main/js/header.js
@@ -1,9 +1,11 @@
+document.getElementById('logo').style.marginTop = 0;
+
 document.getElementById('menu').addEventListener('click', e=> {
-      document.getElementById('menu').classList.add('not-visible');
-      document.getElementById('exit').classList.remove('not-visible');
+    document.getElementById('exit').classList.remove('not-visible');
+    document.getElementById('menu_block').style.left = "0";
 })
 
 document.getElementById('exit').addEventListener('click', e=> {
-      document.getElementById('exit').classList.add('not-visible');
-      document.getElementById('menu').classList.remove('not-visible');
+    document.getElementById('exit').classList.add('not-visible');
+    document.getElementById('menu_block').style.left = "100%";
 })
---
+++ main/static/main/js/home.js
@@ -0,0 +1,5 @@
+document.getElementById("block_buttons").style.opacity = 1;
+document.getElementById("block_info").style.marginLeft = '50px';
+
+document.getElementById("mobile_block_buttons").style.opacity = 1;
+document.getElementById("mobile_block_info").style.marginLeft = '5%';
